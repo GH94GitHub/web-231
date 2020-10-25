77df53ad951bcaca6552651e843e2db1260d0a2f
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2020-10-25 15:04:42 -0500

    Added completed 'henderson-assignment-1.4.js'

---
+++ week-1/henderson-assignment-1.4.js
@@ -0,0 +1,40 @@
+/*
+=================================
+; Title: Assignment 1.4
+; Author: George Henderson
+; Date: 25 October 2020
+; Description: This program initializes variables
+; with values and displays them concatenated at the end
+;=================================
+*/
+
+// start program 
+
+/* 
+    Expected output:
+
+    George
+    Henderson
+    26
+    906 East 31st ST
+    Lincoln
+    68850
+*/
+
+// variable declaration and assignment
+let firstName = "George";
+let lastName = "Henderson";
+let age = "26";
+let street = "906 East 31st ST";
+let city = "Lincoln";
+let zipCode = 68850;
+
+// output
+console.log(firstName + "\n");
+console.log(lastName + "\n");
+console.log(age + "\n");
+console.log(street + "\n");
+console.log(city + "\n");
+console.log(zipCode);
+
+// end program
