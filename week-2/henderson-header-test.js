cbba07b6321dd53f366fba3707ffdcb8424a10c2
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2020-10-30 20:32:46 -0500

    Added code attribution and comments

--- week-2/henderson-header-test.js
+++ week-2/henderson-header-test.js
@@ -1,3 +1,25 @@
+/*========================
+; Author: George Henderson
+; Date: 30 October 2020
+; Description: Importing and testing the display header function
+;========================*/
+
+/*
+  Expected Output:
+
+  George Henderson
+  Exercise 2.2
+  Date: 10/30/2020
+*/
+
+// start program
+
+
+// variable
 const header = require('../henderson-header.js');
 
+//output
 console.log(header.display("George", "Henderson", "Exercise 2.2"));
+
+
+//end program
