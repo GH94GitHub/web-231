90fb93869044c48b2a9a4acd07c66fc01c2e9196
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2020-10-30 20:54:11 -0500

    Imported header and modified output to show header & full name

--- week-2/henderson-exercise-2.3.js
+++ week-2/henderson-exercise-2.3.js
@@ -1,3 +1,4 @@
+const header = require("../henderson-header.js");
 /*========================
 ; Author: George Henderson
 ; Date: 30 October 2020
@@ -5,22 +6,29 @@
 ; and outputs to console.
 ;========================*/
 
+// start program
+
 /*
   Expected Output:
 
-  George
+  George Henderson
+  Exercise 2.3
+  Date: 10/30/2020
+
+  George Henderson
 */
 
-
-// start program
+// function properties
+myName.george = "George Henderson";
 
 // function
 function myName() {
   return myName.george;
 }
-myName.george = "George";
 
 // output
+console.log(header.display("George", "Henderson", "Exercise 2.3"));
+console.log("\n");
 console.log(myName());
 
 // end program
